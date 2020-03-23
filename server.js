bd487fe17c61d9f9a91c0dfed89d8090c3c56391
Robin Jones <RobinJones335@gmail.com>
2020-03-23 16:47:09 -0400

    corrected routes

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var fs = require("fs");
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
 
 // ================================================================================
 // ROUTER
